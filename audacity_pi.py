0825758e9d4a0b83032fba34dc5f4a428e7a3826
tom0521 <tsherwood99@gmail.com>
2019-03-24 00:26:35 -0400

    Finished commenting all functions; Added time delay to the loading animation

--- audacity_pi.py
+++ audacity_pi.py
@@ -5,7 +5,9 @@ import subprocess
 from gpiozero import LED, Button
 from threading import Thread
 
-# Initialize LEDs and Buttons
+""" Code partially adapted from the audacity scripting repo """
+
+""" Initialize LEDs and Buttons """
 LEDS = [ LED(0), LED(1), LED(2) ]
 record_button = Button(3)
 pause_button = Button(4)
@@ -18,15 +20,17 @@ TOFILE = None
 FROMFILE = None
 
 def setup:
+	""" Run audacity and wait 10 seconds for it to start """
 	subprocess.call('audacity')
 	time.sleep(10)
+	""" Set event listener methods for button presses """
 	record_button.when_pressed = record
 	pause_button.when_pressed = pause
 	stop_button.when_pressed = stop
+	""" Check for the script-pipes and open them """
 	if not (os.path.exists(TONAME) and os.path.exists(FROMNAME)):
 		print("mod-script-pipe not found...")
 		sys.exit()
-
 	print(" -- Pipes found!")
 	TOFILE = open(TONAME, 'w')
 	print(" -- To Pipe opened")
@@ -56,30 +60,36 @@ def do_command(command):
 	return response
 
 def leds_off():
+	""" Turn all LEDs off """
 	for led in LEDS:
 		led.off()
 
 def loading_anim(thread):
+	""" Run through LED animation while the thread is active """
 	leds_off()
 	while thread.is_active():
 		for led in LEDS:
 			led.on()
+			time.sleep(0.5)
 		for led in LEDS:
 			led.off()
+			time.sleep(0.5)
 		for led in reversed(LEDS):
 			led.on()
+			time.sleep(0.5)
 		for led in reversed(LEDS):
 			led.off()
+			time.sleep(0.5)
 		
-# load the audacity script-pipe
-
 def save():
+	""" Saves the current project with filename of its timestamp """
 	do_command('SaveProject2: Filename=' + time.asctime(time.localtime(time.time())))
 	# TODO
 	# connect to server
 	# save file on server
 
 def reset():
+	""" Stop recording, save and close the project then open a new one """
 	do_command('PlayStop:')
 	save()
 	do_command('Close:')
@@ -87,6 +97,8 @@ def reset():
 
 def stop():
 	""" Stop recording and save the project """
+	"""   While saving play LED animation   """
+	"""  When finished, turn on green LED   """
 	leds_off()
 	save_thread = Thread(target=reset)
 	save_thread.start()
@@ -95,15 +107,18 @@ def stop():
 	LEDS[2].on()
 
 def record():
+	""" Start recording and turn on the Red LED """
 	leds_off()
 	do_command('Record1stChoice:')
 	LEDS[0].on()
 
 def pause():
+	""" Pause recording and turn on the Yellow LED """
 	leds_off()
 	do_command('Pause:')
 	LEDS[1].on()
 
+""" While setting everything up, play loading animation """
 setup_thread = Thread(target=setup)
 setup_thread.start()
 loading_anim(setup_thread)
